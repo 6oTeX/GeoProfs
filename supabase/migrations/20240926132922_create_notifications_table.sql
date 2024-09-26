bb1c718eda9afb5b514eda014d63147683f56bcb
Noud <noudjanssen123@gmail.com>
2024-09-26 16:00:42 +0200

    changed migrations order

---
+++ supabase/migrations/20240926132922_create_notifications_table.sql
@@ -0,0 +1,20 @@
+
+
+CREATE TYPE public.notification_type AS ENUM (
+    'info',
+    'warning',
+    'error'
+);
+
+CREATE TABLE public.notifications (
+    id serial,
+    user_id uuid null,
+    message text not null,
+    notification_type public.notification_type null default 'info'::notification_type,
+    created_at timestamp with time zone null default now(),
+    is_read boolean null default false,
+    payload jsonb null,
+    receiver_id uuid[] not null,
+    constraint notifications_pkey primary key (id),
+    constraint notifications_user_id_fkey foreign key (user_id) references auth.users (id)
+) tablespace pg_default;
---
+++ supabase/migrations/20240926138128_create_user-project-relation_table.sql
@@ -0,0 +1,10 @@
+create table
+  public.user_project_relations (
+    id uuid not null default gen_random_uuid (),
+    created_at timestamp with time zone not null default now(),
+    project_id uuid not null,
+    user_id uuid not null,
+    constraint user_project_relations_pkey primary key (id),
+    constraint user_project_relations_project_id_fkey foreign key (project_id) references projects (id),
+    constraint user_project_relations_user_id_fkey foreign key (user_id) references profiles (id)
+  ) tablespace pg_default;
