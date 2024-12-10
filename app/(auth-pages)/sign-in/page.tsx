98f1fc1c041b704390d8ab9782d60044246a9d7d
Jorran <jorranhoukes1@gmail.com>
2024-12-10 10:51:26 +0100

    feat: Navigation with profile info

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -10,10 +10,10 @@ export default function Login({ searchParams }: { searchParams: Message }) {
     <form className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
-        </Link>
+        Don't have an account?
+        <a className="text-foreground font-medium underline">
+          Ask your admin for an account
+        </a>
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <Label htmlFor="email">Email</Label>
