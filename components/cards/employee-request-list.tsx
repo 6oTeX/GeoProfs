dfe914e51cf3ba9fce2f8d441fabd2deb622c974
Noud <noudjanssen123@gmail.com>
2025-01-15 13:30:56 +0100

    Merge Hotfix

--- components/cards/employee-request-list.tsx
+++ components/cards/employee-request-list.tsx
@@ -13,6 +13,7 @@ export default async function EmployeeRequestList() {
             // Parse dates from returnData into Date objects.
             startDate: new Date(element.start_date),
             endDate: new Date(element.end_date),
+            createdAtDate: new Date(element.created_at)
         }));
     
         // Separate into active, upcoming, and past requests.
@@ -37,49 +38,13 @@ export default async function EmployeeRequestList() {
         sortedRequests = [
             activeRequests.length > 0 ? { ...separator, type: 'active' } : null,
             ...activeRequests,
-            activeRequests.length > 0 && upcomingRequests.length > 0 ? { ...separator, type: 'upcoming' } : null,
+            upcomingRequests.length > 0 ? { ...separator, type: 'upcoming' } : null,
             ...upcomingRequests,
-            upcomingRequests.length > 0 && pastRequests.length > 0 ? { ...separator, type: 'past' } : null,
+            pastRequests.length > 0 ? { ...separator, type: 'past' } : null,
             ...pastRequests,
         ].filter(Boolean);
+        
     }
-
-    const parsedRequests = employeeRequestList.returnData.map((element) => ({
-        ...element,
-        // Parse dates from returnData into Date objects.
-        startDate: new Date(element.start_date),
-        endDate: new Date(element.end_date),
-        createdAtDate: new Date(element.created_at),
-    }));
-
-    // Separate into active, upcoming, and past requests.
-    const activeRequests = parsedRequests.filter(
-        (element) => element.startDate <= currentDate && element.endDate >= currentDate
-    );
-    const upcomingRequests = parsedRequests.filter(
-        (element) => element.startDate > currentDate
-    );
-    const pastRequests = parsedRequests.filter(
-        (element) => element.endDate < currentDate
-    );
-
-    // Sort each list by startDate.
-    activeRequests.sort((a, b) => a.startDate - b.startDate);
-    upcomingRequests.sort((a, b) => a.startDate - b.startDate);
-    pastRequests.sort((a, b) => a.startDate - b.startDate);
-
-    const separator = { isSeparator: true, type: null };
-
-    // Place all in one list with seperators between if there is a request in each group
-    const sortedRequests = [
-        activeRequests.length > 0 ? { ...separator, type: 'active' } : null,
-        ...activeRequests,
-        upcomingRequests.length > 0 ? { ...separator, type: 'upcoming' } : null,
-        ...upcomingRequests,
-        pastRequests.length > 0 ? { ...separator, type: 'past' } : null,
-        ...pastRequests,
-    ].filter(Boolean);
-
     // Render the requests and separators.
     return (
         <div className="flex flex-col gap-2">
