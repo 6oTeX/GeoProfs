ad27fcc3c63c22d4a24a90b8619dc70abcea4fc1
Jorran <jorranhoukes1@gmail.com>
2025-01-28 11:12:28 +0100

    + Added metrics component

---
+++ stories/DashboardMetrics.stories.tsx
@@ -0,0 +1,8 @@
+import DashboardMetrics from "../components/dashboard-metrics"
+
+export default {
+  title: "Components/DashboardMetrics",
+  component: DashboardMetrics,
+}
+
+export const Default = () => <DashboardMetrics />
