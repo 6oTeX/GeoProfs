3c2d42f75be97bb8e897c95cfed04374ff41bfcf
Jorran <jorranhoukes1@gmail.com>
2025-01-28 11:12:10 +0100

    * Updated Dummy data Calendar story

--- stories/Calender.stories.tsx
+++ stories/Calender.stories.tsx
@@ -13,44 +13,51 @@ type Story = StoryObj<typeof Calender>;
 
 // Sample events data with names
 const events = {
-  "2025-01-02": {
-    furloughNames: ["David"],
-    sickNames: ["Eve", "Frank"],
-    team: "ICT",
-  },
-  "2025-01-03": {
+  "2025-01-02": [
+    {
+      team: "ICT",
+      furloughNames: ["David"],
+      sickNames: ["Eve", "Frank"],
+    },
+    {
+      team: "Finance",
+      furloughNames: ["Alice", "Jane"],
+      sickNames: ["Bob"],
+    },
+  ],
+  "2025-01-03": [{
     furloughNames: ["Alice", "Bob"],
     sickNames: ["Charlie"],
     team: "HRM",
-  },
-  "2025-01-05": {
+  }],
+  "2025-01-05": [{
     furloughNames: ["David", "Eve", "Frank", "Alice", "Bob", "Charlie"],
     sickNames: ["Dave", "Jake"],
     team: "Finance",
-  },
-  "2025-01-07": {
+  }],
+  "2025-01-07": [{
     furloughNames: ["David", "Eve"],
     sickNames: ["Dave", "Jake"],
     team: "Finance",
-  },
-  "2025-01-08": {
+  }],
+  "2025-01-08": [{
     furloughNames: ["David", "Bob", "Charlie"],
     sickNames: ["Dave", "Jake"],
     team: "ICT",
-  },
+  }],
 };
 
 // Dynamically calculate the counts
 const processedEvents = Object.fromEntries(
-  Object.entries(events).map(([date, { furloughNames, sickNames, team }]) => [
+  Object.entries(events).map(([date, eventArray]) => [
     date,
-    {
+    eventArray.map(({ furloughNames, sickNames, team }) => ({
       furlough: furloughNames.length,
       sick: sickNames.length,
       furloughNames,
       sickNames,
       team,
-    },
+    })),
   ]),
 );
 
