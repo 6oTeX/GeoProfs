dc070c06b37540f9c93969578b11f6f989e6f749
Jorran <jorranhoukes1@gmail.com>
2025-01-30 09:51:17 +0100

    + Added Auth test

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,24 @@
+describe("Auth login", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000");
+  });
+  it("Log-in", () => {
+    cy.get(".h-20 > :nth-child(3) > .inline-flex").click();
+    cy.get('[placeholder="info@geoprofs.nl"]').type("user1@geoprofs.com");
+    cy.get('[placeholder="Jouw wachtwoord"]').type("password123");
+    cy.get(".\\[\\&\\>input\\]\\:mb-3 > .inline-flex").click();
+
+    cy.contains("Welkom terug");
+  });
+
+  it("Log-out", () => {
+    cy.get(".h-20 > :nth-child(3) > .inline-flex").click();
+    cy.get('[placeholder="info@geoprofs.nl"]').type("user1@geoprofs.com");
+    cy.get('[placeholder="Jouw wachtwoord"]').type("password123");
+    cy.get(".\\[\\&\\>input\\]\\:mb-3 > .inline-flex").click();
+    cy.contains("Welkom terug");
+    cy.get(".h-8").click();
+    cy.contains("Uitloggen").click();
+    cy.contains("Login");
+  });
+});
